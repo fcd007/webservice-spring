6e133a99bd062572e92103a3957ce472d542dead
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-08-24 00:11:51 -0300

    feat(adicionado projeto boileplate inicial spring): iniciado projeto spring boot

---
+++ src/main/java/br/dev/dantas/webservice/WebserviceSpringApplication.java
@@ -0,0 +1,13 @@
+package br.dev.dantas.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceSpringApplication.class, args);
+	}
+
+}
